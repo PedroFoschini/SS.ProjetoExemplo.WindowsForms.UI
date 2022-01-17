8536ebab496d8b45ea1d0838a27befcc6763ff91
Pedro Foschini <plfoschini@gmail.com>
2022-01-17 12:24:47 -0300

    Add project files.

---
+++ SS.ProjetoExemplo.WindowsForms.UI/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SS.ProjetoExemplo.WindowsForms.UI
+{
+    partial class FFormularioInicial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNomeFornecedor = new System.Windows.Forms.Label();
+            this.txtNomeFornecedor = new System.Windows.Forms.TextBox();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.txtNomeContato = new System.Windows.Forms.TextBox();
+            this.lblContato = new System.Windows.Forms.Label();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.dgvFornecedores = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFornecedores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNomeFornecedor
+            // 
+            this.lblNomeFornecedor.AutoSize = true;
+            this.lblNomeFornecedor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeFornecedor.Location = new System.Drawing.Point(12, 9);
+            this.lblNomeFornecedor.Name = "lblNomeFornecedor";
+            this.lblNomeFornecedor.Size = new System.Drawing.Size(107, 13);
+            this.lblNomeFornecedor.TabIndex = 0;
+            this.lblNomeFornecedor.Text = "Nome Fornecedor";
+            // 
+            // txtNomeFornecedor
+            // 
+            this.txtNomeFornecedor.Location = new System.Drawing.Point(15, 25);
+            this.txtNomeFornecedor.Name = "txtNomeFornecedor";
+            this.txtNomeFornecedor.Size = new System.Drawing.Size(305, 20);
+            this.txtNomeFornecedor.TabIndex = 1;
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEndereco.Location = new System.Drawing.Point(12, 59);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(61, 13);
+            this.lblEndereco.TabIndex = 0;
+            this.lblEndereco.Text = "Endereço";
+            // 
+            // txtNomeContato
+            // 
+            this.txtNomeContato.Location = new System.Drawing.Point(15, 125);
+            this.txtNomeContato.Name = "txtNomeContato";
+            this.txtNomeContato.Size = new System.Drawing.Size(305, 20);
+            this.txtNomeContato.TabIndex = 1;
+            // 
+            // lblContato
+            // 
+            this.lblContato.AutoSize = true;
+            this.lblContato.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContato.Location = new System.Drawing.Point(12, 109);
+            this.lblContato.Name = "lblContato";
+            this.lblContato.Size = new System.Drawing.Size(105, 13);
+            this.lblContato.TabIndex = 0;
+            this.lblContato.Text = "Nome do Contato";
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(15, 75);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(495, 20);
+            this.txtEndereco.TabIndex = 1;
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefone.Location = new System.Drawing.Point(12, 161);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(57, 13);
+            this.lblTelefone.TabIndex = 0;
+            this.lblTelefone.Text = "Telefone";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(15, 177);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(149, 20);
+            this.txtTelefone.TabIndex = 1;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(15, 214);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(149, 57);
+            this.btnSalvar.TabIndex = 2;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // dgvFornecedores
+            // 
+            this.dgvFornecedores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFornecedores.Location = new System.Drawing.Point(15, 288);
+            this.dgvFornecedores.Name = "dgvFornecedores";
+            this.dgvFornecedores.Size = new System.Drawing.Size(773, 150);
+            this.dgvFornecedores.TabIndex = 3;
+            // 
+            // FFormularioInicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvFornecedores);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtNomeContato);
+            this.Controls.Add(this.txtNomeFornecedor);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.lblContato);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.lblNomeFornecedor);
+            this.Name = "FFormularioInicial";
+            this.Text = "Tela de Cadastro de Fornecedor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFornecedores)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNomeFornecedor;
+        private System.Windows.Forms.TextBox txtNomeFornecedor;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.TextBox txtNomeContato;
+        private System.Windows.Forms.Label lblContato;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.DataGridView dgvFornecedores;
+    }
+}
+
